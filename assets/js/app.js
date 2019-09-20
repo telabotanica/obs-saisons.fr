198ff59fde534a23933fbb71e72b56af229b84c1
ya-web <idir.alliche@gmail.com>
2019-09-20 11:07:06 +0200

    Templates,styles,images,fonts/ like initial commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,10 +7,19 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 // require('../css/app.css');
-require('../css/reset.css');
+require('../css/lib/reset.css');
+require('../css/lib/tools.css');
+require('../css/font-faces.css');
 require('../css/ods-common.css');
-require('../css/ods.css');
-require('../css/ods-saisie-obs.css');
+require('../css/layout/form.css');
+require('../css/layout/overlay.css');
+require('../css/layout/saisie-obs.css');
+require('../css/layout/menu.css');
+require('../css/layout/topbar.css');
+require('../css/layout/footer.css');
+require('../css/pages/accueil.css');
+require('../css/pages/stations.css');
+require('../css/pages/news.css');
 require('../js/styles.js');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
--- webpack.config.js
+++ webpack.config.js
@@ -77,6 +77,7 @@ Encore
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
     //.addEntry('admin', './assets/js/admin.js')
+
 ;
 
 module.exports = Encore.getWebpackConfig();
