c6d8a43b17affa760a9643d23685854f6b605b3b
ya-web <idir.alliche@gmail.com>
2019-10-09 17:13:55 +0200

    Routes annotations, refactor PagesController
    Le routage se faisait avec routes.yaml, désormais avec annotations
    Refactorisation de la génération de rendering des pages dans le
    controleur

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -30,7 +30,7 @@ class PagesController extends AbstractController
         'station-page' => 'Page de la station',
     ];
 
-	/*
+	/**
 	 * Index action.
 	 *
 	 * @param Request $request
@@ -42,161 +42,136 @@ class PagesController extends AbstractController
 		return $this->render('pages/accueil.html.twig');
 	}
 
-	/*
+    /**
      * @param Request $request
      *
      * @Route("/apropos", name="apropos")
      */
     public function aproposPage(Request $request)
     {
-        return $this->render('pages/apropos.html.twig',[
-            'breadcrumbs' => $this->getBreadcrumbs($request)
-        ]);
+        return $this->basicPageRenderer($request);
     }
 
 
-    /*
+    /**
      * @param Request $request
      *
      * @Route("/actualites", name="actualites")
      */
     public function actualitesPage(Request $request)
     {
-        return $this->render('pages/actualites.html.twig',[
-            'breadcrumbs' => $this->getBreadcrumbs($request)
-        ]);
+        return $this->basicPageRenderer($request);
     }
 
-    /*
+    /**
      * @param Request $request
      *
      * @Route("/actualites/actu", name="actu")
      */
     public function actuPage(Request $request)
     {
-        return $this->render('pages/actu.html.twig',[
-            'breadcrumbs' => $this->getBreadcrumbs($request)
-        ]);
+        return $this->basicPageRenderer($request);
     }
 
-    /*
+    /**
      * @param Request $request
      *
      * @Route("/evenements", name="evenements")
      */
     public function evenementsPage(Request $request)
     {
-        return $this->render('pages/evenements.html.twig',[
-            'breadcrumbs' => $this->getBreadcrumbs($request)
-        ]);
+        return $this->basicPageRenderer($request);
     }
 
-    /*
+    /**
      * @param Request $request
      *
      * @Route("/evenements/evenement", name="evenement")
      */
     public function evenementPage(Request $request)
     {
-        return $this->render('pages/evenement.html.twig',[
-            'breadcrumbs' => $this->getBreadcrumbs($request)
-        ]);
+        return $this->basicPageRenderer($request);
     }
 
-    /*
+    /**
      * @param Request $request
      *
      * @Route("/especes", name="especes")
      */
     public function especesPage(Request $request)
     {
-        return $this->render('pages/especes.html.twig',[
-            'breadcrumbs' => $this->getBreadcrumbs($request)
-        ]);
+        return $this->basicPageRenderer($request);
     }
 
-    /*
+    /**
      * @param Request $request
      *
      * @Route("/participer", name="participer")
      */
 	public function participer(Request $request)
 	{
-		return $this->render('pages/participer.html.twig',[
-            'breadcrumbs' => $this->getBreadcrumbs($request)
-        ]);
+        return $this->basicPageRenderer($request);
 	}
 
-	/*
+    /**
 	 * @param Request $request
 	 *
 	 * @Route("/participer/station-obs", name="station-obs")
 	 */
 	public function stationObs(Request $request)
 	{
-		return $this->render('pages/station-obs.html.twig',[
-            'breadcrumbs' => $this->getBreadcrumbs($request)
-        ]);
+        return $this->basicPageRenderer($request);
 	}
 
-	/*
+    /**
 	 * @param Request $request
 	 *
 	 * @Route("/participer/station-obs/station-page", name="station-page")
 	 */
 	public function stationPage(Request $request)
 	{
-		return $this->render('pages/station-page.html.twig',[
-            'breadcrumbs' => $this->getBreadcrumbs($request)
-        ]);
+        return $this->basicPageRenderer($request);
 	}
 
-    /*
+    /**
      * @param Request $request
      *
      * @Route("/resultats", name="resultats")
      */
     public function resultatsPage(Request $request)
     {
-        return $this->render('pages/resultats.html.twig',[
-            'breadcrumbs' => $this->getBreadcrumbs($request)
-        ]);
+        return $this->basicPageRenderer($request);
     }
 
-    /*
+    /**
      * @param Request $request
      *
      * @Route("/outils-ressources", name="outils-ressources")
      */
     public function outilsRessourcesPage(Request $request)
     {
-        return $this->render('pages/outils-ressources.html.twig',[
-            'breadcrumbs' => $this->getBreadcrumbs($request)
-        ]);
+        return $this->basicPageRenderer($request);
     }
 
-    /*
+    /**
      * @param Request $request
      *
      * @Route("/relais", name="relais")
      */
     public function relaisPage(Request $request)
     {
-        return $this->render('pages/relais.html.twig',[
-            'breadcrumbs' => $this->getBreadcrumbs($request)
-        ]);
+        return $this->basicPageRenderer($request);
     }
-
-
-
 
     /*
      * @param Request $request
      *
-     * @Return $bc
+     * @return $bc
+     *
      */
-	public function getBreadcrumbs(Request $request)
+    public function basicPageRenderer(Request $request)
     {
+        //breadcrumbs
         $currentRoute = $request->attributes->get('_route');
         $currentUrl = $this->get('router')->generate($currentRoute, array(), true);
         $urlParts = explode('/' , $currentUrl );
@@ -213,8 +188,14 @@ class PagesController extends AbstractController
                 $bc = array();
             }
         }
-        return $bc;
+
+        //render page
+        return $this->render('pages/'.$currentRoute.'.html.twig', [
+            'breadcrumbs' => $bc,
+            'route' => $currentRoute
+        ]);
     }
+
 
 }
 
