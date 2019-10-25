8cbf1f1702ddaed7f8ce28c68da1e5a7e4cba6ae
ya-web <idir.alliche@gmail.com>
2019-10-25 16:42:49 +0200

    Added JS for dropdowns & other hide/show features

--- assets/js/styles.js
+++ assets/js/styles.js
@@ -15,8 +15,61 @@ function toggleMenuSmallDevices(){
 	});
 }
 
+function toggleOverlay() {
+	$('a.open').on('click', function (event) {
+		event.preventDefault();
+		var id = $(this).data('open');
+		$('.overlay.' + id).removeClass('hidden');
+	});
+
+	$('a.bt-annuler').on('click', function (event) {
+		event.preventDefault();
+		$(this).closest('.overlay').addClass('hidden');
+	});
+}
+
+function toggleDropdown() {
+	$('.dropdown-toggle').off('click').on('click', function (event) {
+		event.preventDefault();
+		$(this).siblings('.dropdown-list').toggleClass('hidden');
+	})
+}
+
+function toggleCalendar() {
+	$('.item-heading-dropdown').off('click').on('click', function (event) {
+		event.preventDefault();
+		var id = $(this).closest('.list-cards-item').data('id');
+
+		$(this).toggleClass('right-arrow-orange-icon down-arrow-icon');
+		$('.periods-calendar[data-id="' + id + '"]').toggleClass('hidden');
+	});
+
+	$('.table-mask-button').off('click').on('click', function (event) {
+		event.preventDefault();
+		$(this).closest('.periods-calendar').addClass('hidden');
+	});
+}
+
+function calendarSwitchDate() {
+	$('.dropdown-link').on('click', function (event) {
+		event.preventDefault();
+		var thisCalendar = $(this).closest('.dropdown'),
+			date = $(this).text();
+
+		$('.active-year', thisCalendar).text(date);
+		$('.dropdown-link.hidden', thisCalendar).removeClass('hidden');
+		$(this).addClass('hidden');
+		$('.dropdown-list', thisCalendar).addClass('hidden');
+	})
+	// todo: throw event to get that years datas or call datas with ajax
+}
+
 $( document ).ready( function() {
 	toggleMenuSmallDevices();
+	toggleOverlay();
+	toggleDropdown();
+	toggleCalendar();
+	calendarSwitchDate();
 	addModsTouchClass();
 	$(window).on('resize', addModsTouchClass());
 });
