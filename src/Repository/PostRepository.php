58015232298edd069b2493a3cac6781d6534e64a
ya-web <idir.alliche@gmail.com>
2019-11-28 15:46:54 +0100

    Fix upgraded and renamed news into post entity

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -5,7 +5,7 @@ namespace App\Repository;
 use App\Entity\Post;
 use App\Service\PostsGenerator;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * Class PostRepository.
@@ -30,7 +30,7 @@ class PostRepository extends ServiceEntityRepository
     /**
      * PostRepository constructor.
      */
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Post::class);
         $this->category = null;
