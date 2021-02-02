00a17cb5a86f1962fd6b4057512cdced3b73bde5
kstefanini <killian@stefanini.fr>
2021-02-02 18:33:53 +0100

    Add sleep to slow down mail sending
    Fix wording
    Remove debug stuff

--- src/Command/OdsImportUsersCommand.php
+++ src/Command/OdsImportUsersCommand.php
@@ -207,21 +207,20 @@ class OdsImportUsersCommand extends Command
 
                 $this->mailer->send(
                     'contact@obs-saisons.fr',
-                    'killian.stefanini.tb@gmail.com',
-//                    $importedUser->getEmail(),
-                    'Merci de réinitialiser votre mot de passe',
+                    $importedUser->getEmail(),
+                    'Merci de réinitialiser votre mot de passe - obs-saisons.fr',
                     $message
                 );
 
-                $io->text('...Ok.');
+                $io->text('...Ok. Sleeping for few seconds');
+
+                sleep(3);
 
                 // flushing each time to avoid sending mail and lost tokens in case of crash
                 $this->em->flush();
-
-                return 0;
             }
 
-            $io->success('Done sending emails!');
+            $io->success('Done sending emails! Users imported \o/');
         }
 
         return 0;
