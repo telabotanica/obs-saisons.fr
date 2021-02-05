d3755102388ed2943970d81a35b9578043b9e504
ya-web <idir.alliche@gmail.com>
2021-02-05 11:31:58 +0100

    Refactor page edition form init

--- assets/js/app.js
+++ assets/js/app.js
@@ -90,7 +90,6 @@ $( document ).ready( function() {
     onOpenOverlay(placesAutocomplete);
     onCloseOverlay();
     stationMapDisplay();
-    initFormEditPage();
     if(0 < $adminDeleteUser.length) {
         userDeleteAdminConfirm();
     }
@@ -904,24 +903,6 @@ function onDeleteButton(subject) {
             event.preventDefault();
         }
     });
-}
-
-function initFormEditPage() {
-    let $uploadInput = $('.upload-input'),
-        isPageForm = $uploadInput.closest('.saisie-container').hasClass('page'),
-        image = $uploadInput.closest('.form-col').data('image');
-
-    if (isPageForm && valOk(image) && '' !== image) {
-        $('.upload-zone-placeholder').addClass('hidden');
-        $('img.placeholder-img').addClass('obj').attr('src', image);
-    }
-
-    let $places = $('.ods-places');
-
-    if (0 < $places.length && valOk($places.val())) {
-        $places.siblings('button.ap-input-icon').toggle();
-    }
-
 }
 
 function userDeleteAdminConfirm() {
