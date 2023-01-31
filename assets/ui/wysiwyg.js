fa3ae0e23047402109b16ce93f9b597e7962c942
Julien MARTIN <ju.martin@outlook.com>
2023-01-31 15:51:28 +0100

    fix ckeditor issue with some html tags

--- assets/ui/wysiwyg.js
+++ assets/ui/wysiwyg.js
@@ -2,11 +2,12 @@ $( document ).ready( () => {
     const wysiwygs = Array.from( document.querySelectorAll( '.wysiwyg-editor' ) );
 
     if ( wysiwygs.length > 0 ) {
-        import( /* webpackChunkName: "ckeditor" */ '@telabotanica/ckeditor5-build-ods' ).then( ( { default: ClassicEditor } ) => {
+        import( /* webpackChunkName: "ckeditor" */ '@telabotanica/ckeditor5-build-ods-v2' ).then( ( { default: ClassicEditor } ) => {
             wysiwygs.forEach( ( wysiwyg ) => {
                 const textarea    = wysiwyg.querySelector( 'textarea' );
                 textarea.required = false;
 
+                /*
                 let toolbar = [
                     'heading', '|',
                     'bold', 'italic', 'underline', 'link', 'bulletedList', 'numberedList', '|',
@@ -15,12 +16,12 @@ $( document ).ready( () => {
                     'alignment', 'indent', 'outdent', '|',
                     'blockQuote', 'insertTable', 'mediaEmbed', '|',
                     'undo', 'redo'
-                ];
+                ];*/
 
                 let simpleUpload = {};
 
                 if ( wysiwyg.getAttribute( 'data-upload' ) ) {
-                    toolbar = [
+                    let toolbar = [
                         'heading', '|',
                         'bold', 'italic', 'underline', 'link', 'bulletedList', 'numberedList', '|',
                         'fontFamily', 'fontSize', 'fontColor', '|',
@@ -37,9 +38,21 @@ $( document ).ready( () => {
 
                 ClassicEditor
                     .create( textarea, {
-                        toolbar:      toolbar,
+                        // toolbar:      toolbar,
+                        removePlugins: ['Title'],
+                        placeholder: '',
                         simpleUpload: simpleUpload,
                         height:       '500px',
+                        htmlSupport: {
+                            allow: [
+                                {
+                                    name: /.*/,
+                                    attributes: true,
+                                    classes: true,
+                                    styles: true
+                                }
+                            ]
+                        }
                     } )
                     .then( editor => {
                         console.log( Array.from( editor.ui.componentFactory.names() ) );
