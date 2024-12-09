12b89b66e1423b3249932753b6b21086d35c1f57
BGE31-Thomas <tpjarles@gmail.com>
2024-12-09 14:36:42 +0100

    qq corrections

--- assets/ui/location/locality.js
+++ assets/ui/location/locality.js
@@ -157,8 +157,6 @@ OdsPlaces.prototype.onSuggestionSelected = function() {
         lthis.placesLatitude.val(lat);
         lthis.placesLongitude.val(lng);
         var sl = new StationLocation();
-        sl.updateCoordinatesFields();
-        sl.handleCoordinates();
         sl.getAltitude();
         lthis.placesCloseButton.trigger('click');
 
